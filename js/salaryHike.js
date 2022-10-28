34a5fa8901b58cf6c36f533f8fd213388dd94a89
vishnumd91 <vishnudivakaran1@gmail.com>
2022-10-28 21:34:22 +0530

    code cleanup for salary hike problem

--- js/salaryHike.js
+++ js/salaryHike.js
@@ -1,27 +1,23 @@
-let designation = window.prompt("Enter The Designation").toLowerCase();
+let designation = prompt("Enter The Designation").toLowerCase();
 
-let salary = parseInt(window.prompt("Enter The Salary"));
+let salary = parseInt(prompt("Enter The Salary"));
 
 const hikeDecider = (designation, salary) => {
-  let salaryHike;
   if (designation === "manager") {
-    salaryHike = (salary * 20) / 100;
-    salary += salaryHike;
+    salary += salary * 0.2;
     return `${designation} Salary is: ${salary}`;
   } else if (designation === "programmer") {
-    salaryHike = (salary * 15) / 100;
-    salary += salaryHike;
+    salary += salary * 0.15;
     return `${designation} Salary is: ${salary}`;
   } else if (designation === "tester") {
-    salaryHike = (salary * 10) / 100;
-    salary += salaryHike;
+    salary += salary * 0.1;
     return `${designation} Salary is: ${salary}`;
   } else {
-    return "Please Enter the proper Designation";
+    salary += salary * 0.05;
+    return `${designation} Salary is: ${salary}`;
   }
 };
 
 const result = hikeDecider(designation, salary);
-console.log(result);
 
 document.write(`<h1>${result}</h1>`);
